236239c9646c02fca6e9c9e90594129ffbe57e9f
jedpark <hatdoc@naver.com>
2021-06-28 16:40:40 +1000

    i am rich

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.purpleAccent,
+        appBar: AppBar(
+          title: Center(child: Text('I am rich)),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
